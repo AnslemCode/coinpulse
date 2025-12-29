de428fe062e93d1bebc214d3c26ed24819f344d5
AnslemCode <102703239+AnslemCode@users.noreply.github.com>
2025-12-30 00:34:56 +0100

    implement coin overview and trending coins ui and functionality using fetcher utility function

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import Categories from "@/components/home/Categories";
+import CoinOverview from "@/components/home/CoinOverview";
+import {
+  CategoriesFallback,
+  CoinOverviewFallback,
+  TrendingCoinsFallback,
+} from "@/components/home/fallback";
+import TrendingCoins from "@/components/home/TrendingCoins";
+import React, { Suspense } from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main className="main-container">
+      <section className="home-grid">
+        <Suspense fallback={<CoinOverviewFallback />}>
+          <CoinOverview />
+        </Suspense>
+
+        <Suspense fallback={<TrendingCoinsFallback />}>
+          <TrendingCoins />
+        </Suspense>
+      </section>
+      <section className="w-full mt-7 space-y-4">
+        <Suspense fallback={<CategoriesFallback />}>
+          <Categories />
+        </Suspense>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
---
+++ lib/coingecko.actions.ts
@@ -0,0 +1,42 @@
+"use server";
+
+import queryString from "query-string";
+
+const BASE_URL = process.env.COINGECKO_BASE_URL;
+const API_KEY = process.env.COINGECKO_API_KEY;
+
+if (!BASE_URL || !API_KEY) {
+  throw new Error("Missing COINGECKO_BASE_URL or COINGECKO_API_KEY");
+}
+
+export async function fetcher<T>(
+  endpoint: string,
+  params?: QueryParams,
+  revalidate = 60
+): Promise<T> {
+  const url = queryString.stringifyUrl(
+    {
+      url: `${BASE_URL}/${endpoint}`,
+      query: params,
+    },
+    { skipEmptyString: true, skipNull: true }
+  );
+
+  const response = await fetch(url, {
+    headers: {
+      "x-cg-pro-api-key": API_KEY,
+      "Content-type": "application/json",
+    } as Record<string, string>,
+    next: { revalidate },
+  });
+
+  if (!response.ok) {
+    const errorBody: CoinGeckoErrorBody = await response.json().catch(() => {});
+
+    throw new Error(
+      errorBody.error || response.statusText || "Something went wrong"
+    );
+  }
+
+  return response.json();
+}
